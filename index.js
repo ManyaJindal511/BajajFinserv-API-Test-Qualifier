7a4c0e7e03b57a1e70527e0d717bda24beba1baa
MANYAJINDAL511 <jindalmanya511@gmail.com>
2026-02-10 14:21:16 +0530

    Adding files

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.post("/bfhl", async (req, res) => {
     const prompt = `Answer in ONE WORD only.\nQuestion: ${body[key]}`;
 
     const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         contents: [
           {
